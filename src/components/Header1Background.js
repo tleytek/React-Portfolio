069cf5c2bddf8eb5c0c9e88498a5cf019b7985b0
Mateus Koehler <mateus@mateuskoehler.com>
2019-03-12 22:04:58 -0400

    removed unnecessary component

--- src/components/Header1Background.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Header1Background = styled.div`
-  border-radius: 25px;
-  background: rgba(255, 255, 255, 0.5);
-  align-self: center;
-`;
-
-export default Header1Background;
--- src/components/Header3Background.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Header3Background = styled.div`
-  background: rgba(255, 255, 255, 0.5);
-  border-radius: 0px 0px 25px 25px;
-  align-self: center;
-`;
-
-export default Header3Background;
